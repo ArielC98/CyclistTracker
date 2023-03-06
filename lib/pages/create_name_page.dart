9d765fc5e68a3c5e26ab8a736d3706f6942c2182
Ariel <ariel.calderon@epn.edu.ec>
2023-03-06 01:31:18 -0500

    interfaz de creacion de usaurio mejorada

--- lib/pages/create_name_page.dart
+++ lib/pages/create_name_page.dart
@@ -13,6 +13,19 @@ class _CreateNamePageState extends State<CreateNamePage> {
   TextEditingController userController = TextEditingController(text: "");
   TextEditingController passwordController = TextEditingController(text: "");
   FirebaseAuth auth = FirebaseAuth.instance;
+  bool isChecked = false;
+
+  Color getColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+      MaterialState.hovered,
+      MaterialState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Colors.blue;
+    }
+    return Colors.blue;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +47,17 @@ class _CreateNamePageState extends State<CreateNamePage> {
               hintText: 'Ingrese la contraseña',
             ),
           ),
+          CheckboxListTile(
+            title: const Text("Usuario administrador"),
+            checkColor: Colors.white,
+            value: isChecked,
+            controlAffinity: ListTileControlAffinity.leading,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value!;
+              });
+            },
+          ),
           ElevatedButton(
               onPressed: () async {
                 try {
@@ -42,13 +66,18 @@ class _CreateNamePageState extends State<CreateNamePage> {
                           email: userController.text,
                           password: passwordController.text)
                       .then((_) {
-                    Navigator.pop(context);
+                    showAlertDialog(
+                        context, "Éxito", "Usuario creado exitosamente");
+                    userController.text = "";
+                    passwordController.text = "";
                   });
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'contraseña debil') {
-                    print('La contraseña es muy debil');
+                    showAlertDialog(
+                        context, "Error", "La contraseña es muy debil");
                   } else if (e.code == 'email-already-in-use') {
-                    print('El correo ingresado ya esta registrado');
+                    showAlertDialog(context, "Error",
+                        "El correo ingresado ya esta registrado");
                   }
                 } catch (e) {
                   print(e);
@@ -59,4 +88,32 @@ class _CreateNamePageState extends State<CreateNamePage> {
       ),
     );
   }
+}
+
+//Cuadro de alertas
+showAlertDialog(BuildContext context, String title, String message) {
+  // Crear el boton
+  Widget okButton = TextButton(
+    child: const Text("OK"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+
+  // Crear el cuadro
+  AlertDialog alert = AlertDialog(
+    title: Text(title),
+    content: Text(message),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // mostrar el dialogo
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
